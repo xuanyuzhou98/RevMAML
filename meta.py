d81619b9056142342fcc22d3667a91aa19cfa422
Xuanyu Zhou <xuanyu_zhou@berkeley.edu>
2020-10-31 23:08:27 +0800

    Update meta.py

--- meta.py
+++ meta.py
@@ -32,8 +32,8 @@ class Meta(nn.Module):
         self.update_step = args.update_step
         self.update_step_test = args.update_step_test
 
-        self.net = iRevNet([4,4,4], [1,2,2], self.n_way, nChannels=None, init_ds=0,
-                 dropout_rate=0.1, affineBN=True, in_shape=[28,28,1], mult=4)
+        self.net = iRevNet([4,4,4], [1,2,2], self.n_way, nChannels=[16,64,256], init_ds=0,
+                 dropout_rate=0.1, affineBN=True, in_shape=[1,28,28], mult=4)
         # self.net = Learner(config, args.imgc, args.imgsz)
         self.meta_optim = optim.Adam(self.net.parameters(), lr=self.meta_lr)
 
